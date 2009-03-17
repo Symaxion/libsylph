b3db238c3e85e59254eb4a9f9a4e10d39d65c527
SeySayux <SeySayux@79106967-043d-4803-9355-89110ef6a03e>
2009-03-17 14:35:43 +0000

    Added documentation for ArrayIterator
    git-svn-id: https://opensvn.csie.org/libsylph/trunk/sylph@21 79106967-043d-4803-9355-89110ef6a03e

--- src/Sylph/Core/ArrayIterator.h
+++ src/Sylph/Core/ArrayIterator.h
@@ -17,12 +17,44 @@ class Array;
 
 SYLPH_PUBLIC
 
+/**
+ * This class provides an @c Iterator for @c Array.
+ * This iterator is not fail-fast, multithreading
+ * is therefore not yet supported!
+ * An easier way to create an instance of this
+ * iterator that is collection-independant, is to
+ * use @c Array::iterator().
+ * This iterator does not support modifing the
+ * collection while it iterates over it.
+ */
 template<class T>
 class ArrayIterator : public virtual Iterator<T> {
 public:
+    /**
+     * Default constructor.
+     * @param array Array to iterate over.
+     */
     ArrayIterator(Array<T> & array) : ar(&array) {}
+    /**
+     * Default destructor
+     *
     virtual ~ArrayIterator() {}
+    /**
+     * Check if this iterator has any more entries
+     * @return true if this iterator has more entries, false
+     * otherwise
+     * @see Array::length
+     */
     bool hasNext() const;
+    /**
+     * Returns the next entry in the array and moves this
+     * iterator one place forward. 
+     * @throw ArrayException if trying to return an entry
+     * outside the bounds of the Array.
+     * @return the next entry in the array.
+     * @see ArrayIterator::hasNext
+     * @see Array::operator[]
+     */
     const T & next() const;
     bool hasPrevious() const;
     const T & previous() const;
