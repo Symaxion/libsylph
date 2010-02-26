79b5e00d11c1d669c74d87a050cb011e4a5b6eea
Frank Erens <frank@synthi.net>
2010-02-26 18:58:39 +0100

    Added SylphTest.h

---
+++ test/SylphTest.h
@@ -0,0 +1,28 @@
+/*
+ * LibSylph Class Library
+ * Copyright (C) 2009 Frank "SeySayux" Erens <seysayux@gmail.com>
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the LibSylph Pulbic License as published
+ * by the LibSylph Developers; either version 1.0 of the License, or
+ * (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the LibSylph
+ * Public License for more details.
+ *
+ * You should have received a copy of the LibSylph Public License
+ * along with this Library, if not, contact the LibSylph Developers.
+ * 
+ * Created on 20 december 2009, 17:07
+ */
+
+#ifndef SYLPHTEST_H_
+#define	SYLPHTEST_H_
+#include <gtest/gtest.h>
+
+#define SYLPH_STUB_TEST ADD_FAILURE() << "FIXME: this is a stub testcase!"
+
+#endif	/* SYLPHTEST_H_ */
+
