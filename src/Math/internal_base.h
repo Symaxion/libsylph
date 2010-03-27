b84781f1665ccc5d9791a3513c7f649e2dd64d77
Frank Erens <frank@synthi.net>
2010-03-27 09:46:29 +0100

    Created branch `math'. Added simple interface for Math namespace.

---
+++ src/Math/internal_base.h
@@ -0,0 +1,17 @@
+/* 
+ * File:   internal_base.h
+ * Author: Frank "SeySayux" Erens <seysayux@gmail.com>
+ *
+ * Created on 16 maart 2010, 15:23
+ */
+
+#ifndef INTERNAL_BASE_H_
+#define	INTERNAL_BASE_H_
+
+#include "../Core/Common.h"
+
+#define SYLPH_START_MATHNS SYLPH_START_NAMESPACE namespace Math {
+#define SYLPH_END_MATHNS } SYLPH_END_NAMESPACE
+
+#endif	/* INTERNAL_BASE_H_ */
+
