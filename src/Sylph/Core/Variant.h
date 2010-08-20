0e8b81ac9e639836d472110a8b37ca3e121ed8d8
Frank Erens <frank@synthi.net>
2010-08-20 18:12:37 +0200

    Initial checkin for branch 'Variant'

---
+++ src/Sylph/Core/Variant.h
@@ -0,0 +1,49 @@
+/* 
+ * File:   Variant.h
+ * Author: frank
+ *
+ * Created on 20 augustus 2010, 17:40
+ */
+
+#ifndef VARIANT_H_
+#define	VARIANT_H_
+
+#include "Common.h"
+
+SYLPH_BEGIN_NAMESPACE
+
+class PrimType {
+    // temporary. Replace with real Primtype.
+};
+
+Array<Array<bool>> conversionAllowed =
+{
+  // Byt Chr Shr Int Lng Ble Ush Uin Ulg Flt Dbl Arr Str Dic Buf Dat
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1,  0 }, // Byte
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 0 }, // Char
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 0 }, // Short
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 1 }, // Int
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 1 }, // Long
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 0 }, // Bool
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 0 }, // Ushr
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 1 }, // UInt
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 1 }, // Ulng
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 0 , 0 }, // Float
+    { 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 0 , 0 }, // Double
+    { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 1 , 0 }, // Array
+    { 1 , 0 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 1 , 0 , 1 , 1 }, // String
+    { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 0 , 0 }, // Dict
+    { 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 1 , 1 , 0 , 1 , 0 }, // Buffer
+    { 0 , 0 , 0 , 1 , 1 , 0 , 0 , 1 , 1 , 0 , 0 , 0 , 1 , 0 , 0 , 1 }  // Date
+};
+
+class Variant : public virtual Object {
+public:
+private:
+    void* content;
+    PrimType type;
+};
+SYLPH_END_NAMESPACE
+
+#endif	/* VARIANT_H_ */
+
