49a563c13d5756d5deb34c6eae3f2443df2cecdf
Frank Erens <frank@synthi.net>
2012-11-04 14:22:11 +0100

    Intial implementation of properties.

--- src/Sylph/Core/Property.h
+++ src/Sylph/Core/Property.h
@@ -27,18 +27,92 @@
 #define SYLPH_CORE_PROPERTY_H_
 
 #include "Object.h"
+#include "Function.h"
 
 SYLPH_BEGIN_NAMESPACE
 
-class Property {
+// Weak properties
+// read-write weak property
+template<class T, bool B = false>
+class WeakProperty : public WeakProperty<T, true> {
 public:
-    Property();
-    Property(const Property& other);
-    virtual ~Property();
+    virtual void set(const T& t) = 0;
 
-private:
-    
+    T& operator*() { return get(); }
+    T& operator->() { return get(); }
+
+    template<class U>
+    WeakProperty<T,B>& operator=(const U& u) { set(u); return *this; }
+
 };
+
+// read-only weak property
+template<class T>
+class WeakProperty<T, true> {
+    typedef WeakProperty<T, true> Self;
+
+    virtual T& get() { return const_cast<T&>(constify(*this).get()); }
+    virtual const T& get() const = 0;
+
+    const T& operator*() const { return get(); }
+    const T& operator->() const { return get(); }
+
+    template<class U>
+    auto operator+(const U& u) -> decltype(get() + u) {
+        return get() + u;
+    }
+    template<class U>
+    auto operator-(const U& u) -> decltype(get() - u) {
+        return get() - u;
+    }
+    template<class U>
+    auto operator*(const U& u) -> decltype(get() * u) {
+        return get() * u;
+    }
+    template<class U>
+    auto operator/(const U& u) -> decltype(get() / u) {
+        return get() / u;
+    }
+    template<class U>
+    auto operator%(const U& u) -> decltype(get() % u) {
+        return get() % u;
+    }
+};
+
+// Strong properties
+template<class T, bool B = false>
+class Property {
+};
+
+// read-write property
+template<class T>
+class Property<T, false> : public WeakProperty<T, false> {
+public:
+    Property() : value(T()) {}
+    Property(const T& t) : value(t) {}
+    virtual ~Property() {}
+
+    virtual void set(const T& t) { value = t; }
+    virtual const T& get() const { return value; }
+private:
+    T value;
+};
+
+// read-only property
+template<class T>
+class Property<T, true> : public WeakProperty<T, true> {
+public:
+    Property() : value(T()) {}
+    Property(const T& t) : value(t) {}
+    virtual ~Property() {}
+
+    virtual const T& get() const { return value; }
+protected:
+    T value;
+};
+
+#define weak_property struct : public ::Sylph::WeakProperty
+#define property struct : public ::Sylph::Property
 
 SYLPH_END_NAMESPACE
 
