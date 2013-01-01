3dd573e65d9bb5ddc2e6a218e3ec50bc8595889f
Frank Erens <frank@synthi.net>
2013-01-01 14:50:13 +0100

    Updated copyright.

--- cmake/EnableCxx11.cmake
+++ cmake/EnableCxx11.cmake
@@ -1,7 +1,7 @@
 #########################################################################
 #
 # LibSylph Class Library (build script)
-# Copyright (C) 2012 Frank "SeySayux" Erens <seysayux@gmail.com>
+# Copyright (C) 2013 Frank "SeySayux" Erens <seysayux@gmail.com>
 #
 # This software is provided 'as-is', without any express or implied
 # warranty. In no event will the authors be held liable for any damages
--- src/Sylph/IO/BufferedOutputStream.cpp
+++ src/Sylph/IO/BufferedOutputStream.cpp
@@ -1,6 +1,6 @@
 /*
  * LibSylph Class Library
- * Copyright (C) 2012 Frank "SeySayux" Erens <seysayux@gmail.com>
+ * Copyright (C) 2013 Frank "SeySayux" Erens <seysayux@gmail.com>
  *
  * This software is provided 'as-is', without any express or implied
  * warranty. In no event will the authors be held liable for any damages
